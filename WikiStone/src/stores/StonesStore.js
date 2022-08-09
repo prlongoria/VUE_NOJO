1363959ce52c6b6a2737bd305e31b17a798dd493
prlongoria <prlongoria@gmail.com>
2022-08-09 21:59:09 +0200

    Pinia with data from .json in devtools

--- WikiStone/src/stores/StonesStore.js
+++ WikiStone/src/stores/StonesStore.js
@@ -1,19 +1,9 @@
 import { defineStore } from "pinia";
-import  stones  from "./dataStoneFake.json";
-
-export const useStonesStore = defineStore ({
-    id:"StonesStore", options: {
-    
-        state: () =>{
-            return {
-                stones,
-            }
-        },
-        actions: {
-            addData(stoneobject) {
-              
-                this.stones.push(stoneobject) 
-            }
-          }
+import  dataStoneFake  from "./dataStoneFake.json";
+export const useStonesStore = defineStore("StonesStore", {
+    state: () => {
+        return {
+            dataStoneFake
+        }
     }
 })
