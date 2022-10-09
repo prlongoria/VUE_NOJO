ecffdf02a34493ac1ab9956fd800ca7439719e07
Patricia Rodríguez <prlongoria@gmail.com>
2022-10-09 12:41:13 +0200

    background color

--- WikiStone/src/components/my_components/header/SearchBar.vue
+++ WikiStone/src/components/my_components/header/SearchBar.vue
@@ -1,20 +1,40 @@
 <!-- Ponerle funcionalidad -->
 
-<script>
-export default {};
+<script setup>
+import { ref } from "vue";
+let input = ref("");
+//const stones = [];
+
+//  const response =  fetch("http://localhost:8080/api/v1/stone/");
+//          this.stones =  response.json();
+
+// function filteredList() {
+//   return stones.filter((stone) =>
+//     stone.toLowerCase().includes(input.value.toLowerCase())
+//   );
+// }
 </script>
 
 <template>
+  
   <div class="searchBar">
     <input
       class="form-control form-text"
       maxlength="128"
       type="text"
+      v-model="input"
       placeholder="🔎 Busca tu piedra"
       name="search"
       onkeyup="autocompletado()"
     />
   </div>
+
+  <div class="searchBar" v-for="stone in stones" :key="stone.id"></div>
+  <!-- <div class="searchBar" v-for="stone in filteredList()" :key="stone.id"> -->
+    <!-- <p>{{ stone.id }}</p></div> --> 
+  <!-- <div class="stone error" v-if="input && !filteredList().length">
+    <p>Piedra no encontrada</p>
+  </div> -->
 </template>
 
 <style lang="css" scoped>
